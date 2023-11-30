2dce0110ba957d0daa5be5f27cdd2ecaf62b4d8c
Mathieu SCHLEICH <mathieu.schleich@devoteam.com>
2023-11-30 11:36:41 +0100

    Add tests

---
+++ tests/utils/isValidTimeZone.test.js
@@ -0,0 +1,18 @@
+const { isValidTimeZone } = require("../../utils/isValidTimeZone");
+
+describe("isValidTimeZone", () => {
+  test("should return true for a valid timezone", () => {
+    const validTimeZone = "America/New_York";
+    expect(isValidTimeZone(validTimeZone)).toBe(true);
+  });
+
+  test("should return false for an invalid timezone", () => {
+    const invalidTimeZone = "Invalid/Timezone";
+    expect(isValidTimeZone(invalidTimeZone)).toBe(false);
+  });
+
+  test("should return false for an empty timezone", () => {
+    const emptyTimeZone = "";
+    expect(isValidTimeZone(emptyTimeZone)).toBe(false);
+  });
+});
--- utils/isValidTimeZone.js
+++ utils/isValidTimeZone.js
@@ -1,5 +1,15 @@
 const moment = require("moment-timezone");
 
+/**
+ * Checks if the provided time zone is valid.
+ *
+ * @param {string} timezone - The name of the time zone to check.
+ * @returns {boolean} - True if the time zone is valid, otherwise False.
+ * @throws {Error} - Throws an error if the parameter is not a string.
+ * @example
+ * const isValid = isValidTimeZone("America/New_York");
+ * // Returns true if "America/New_York" is a valid time zone.
+ */
 module.exports.isValidTimeZone = (timezone) => {
   return moment.tz.zone(timezone) != null;
 };
